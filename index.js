41109cc69897bb3c922a4f67f7d0131059e4df53
Tushar <kmrtushar01@gmail.com>
2024-02-22 16:09:11 +0530

    switch tab fuction and its listners

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ const grantAccessButton = document.querySelector("grant-Access-button");
 let yourLocationTab = yourWeatherTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 yourLocationTab.classList.add("current-tab");
+
+
+function switchTab(newTab){
+    if(newTab != yourLocationTab){
+        yourWeatherContainer.classList.remove('active');
+        yourWeatherContainer = newTab;
+        yourWeatherContainer.classList.add('active');
+
+        if(!searchWeatherForm.classList.contains('active')){
+            searchWeatherForm.classList.add('active');
+            yourWeatherContainer.classList.remove('active');
+            grantAccessSection.classList.remove('active');
+
+        }
+
+        else{
+            searchWeatherForm.classList.add('active');
+            yourWeatherContainer.classList.remove('active');
+            getSessionStorage();
+        }
+    }
+}
+
+
+yourLocationTab.addEventListener('click' , ()=>{
+    switchTab(yourLocationTab);
+});
+
+
+searchWeatherTab.addEventListener('click' , ()=>{
+    switchTab(searchWeatherTab);
+});
+
+
